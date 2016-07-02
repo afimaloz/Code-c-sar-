a184a8098d6b4374e2fc3f8307d2139d73273c7d
afimaloz <afntoinelbc@yahoo.fr>
2016-07-02 03:06:11 +0200

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -1,11 +1,18 @@
+# Initialisation des valeurs
 mot = input("Ecrivez un MOT:")
 nouveaumot = ""
 ordre = {}
+
+# Boucle 
 for i in range(26): 
+    # Exemple utilisation 3 % 26 
     i_nouvelindice = (i + 3) % 26
     c_nouvelindice = chr(i_nouvelindice + ord('a'))
     c = chr(i + ord('a'))
     ordre[c] = c_nouvelindice
+    
 for c in mot:
     nouveaumot = nouveaumot + ordre[c]
+    
+#Affiche votre mot crypté
 print(nouveaumot)
